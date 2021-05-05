c1b31327dcbece1662ed3f01b6d6da1ec9dd0165
swallows98 <79219483+swallows98@users.noreply.github.com>
2021-05-05 14:22:22 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var button = document.getElementById('btn-download');
+button.addEventListener('click', function (e) {
+    var dataURL = canvas.toDataURL('image/png');
+    button.href = dataURL;
+});
